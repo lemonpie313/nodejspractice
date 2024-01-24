13798a200073913d486283bbd3b4509b35a6e6cf
SuhyeonJin <lofi0613@gmail.com>
2024-01-24 21:50:58 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,13 +11,13 @@ function login() {
         id : id.value,
         psword : psword.value,
     };
-    console.log(req);
 
     fetch("/login", {
         method: "POST", //Restful API 메소드
         headers: {
             "Content-Type": "application/json" //보내려는 콘텐츠 타입 
         },
-        body: JSON.stringify(req) //문자열로 변환 후 데이터 전달
-    })
-}
+        body: JSON.stringify(req), //문자열로 변환 후 데이터 전달
+    }).then((res) => res.json()) //응답받은 데이터(받은 아이디 비번) 반환
+    .then((res) => console.log(res)); //res.json() 반환값은 promise. then((res) => console.log(res.json()))의 경우 promise로 반환
+} //따라서 promise 타입은 then으로 접근 가능.
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -11,13 +11,32 @@ const output = { //hello, login을 output 객체로 묶음
     },
 };
 
+const users = {
+    id: ["aaa", "bbb", "ccc"],
+    psword: ["1234","5678","9012"],
+}; //아이디 비번
+
 const process = {
     login: (req,res) => {
-        console.log(req.body); //login.js에서 body로 데이터 전달해서, body로 접근해야됨
+        const id = req.body.id, //login.js body에서 받아온 id, psword
+        psword = req.body.psword;
+        
+        if (users.id.includes(id)){ //아이디 비번 일치여부 확인
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx]==psword){
+                return res.json({ //프론트엔드에 json객체로 응답
+                    success : true,
+                });
+            }
+        }
+        return res.json({
+            success : false,
+            msg : "로그인 실패",
+        });
     },
-}
+};
 
 module.exports = {
     output,
     process,
-}
+};
