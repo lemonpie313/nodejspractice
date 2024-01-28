26b8f27d6c12c2aea0237bf95b4f14ffffbdf1a0
SuhyeonJin <lofi0613@gmail.com>
2024-01-28 15:15:33 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,22 @@
 "use strict";
 
-const id = document.querySelector("#id"), //#id:html에 input id로 부여된 'id'를 갖고오기
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login0); //클릭하면 login 함수 실행
+registerBtn.addEventListener("click", register0);
 
-function login0() {
+function register0() {
     const req = {
         id : id.value,
+        name : name.value,
         psword : psword.value,
+        confirmPsword : confirmPsword.value,
     };
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST", //Restful API 메소드
         headers: {
             "Content-Type": "application/json" //보내려는 콘텐츠 타입 
@@ -21,13 +25,13 @@ function login0() {
     }).then((res) => res.json()) //응답받은 데이터 반환
     .then((res) => {
         if (res.success) {
-            location.href = "/"; //로그인 성공 시 이동할 경로
+            location.href = "/login"; //로그인 성공 시 이동할 경로
         } else {
             alert(res.msg); //로그인 실패 시 팝업 메시지 (home.ctrl의 msg)
         }
 
     })
     .catch((err) => { //에러 발생할 경우 에러 처리
-        console.error(new Error("로그인 중 에러 발생")); //new Error 없애기도 함 
+        console.error(new Error("회원가입 중 에러 발생")); //new Error 없애기도 함 
     });
 } 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -20,7 +20,6 @@ const process = {
     login2: (req,res) => { // ** req.body : post 형식으로 넘어오는 파라미터 담음
         const user = new User(req.body); //req.body(입력된 id비번) > Users.js의 constructor(body)로 넘어감
         const response = user.login1();
-        console.log(response);
         return res.json(response);
     },
 };
